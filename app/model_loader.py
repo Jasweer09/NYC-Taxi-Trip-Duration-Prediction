fbdcbc6ed31a71c25a2991bff2ed3df67d06ffc5
JasweerTadikonda <jasweertadikonda@gmail.com>
2025-07-31 23:04:40 -0400

    Update model_loader.py

--- app/model_loader.py
+++ app/model_loader.py
@@ -1,29 +1,33 @@
-import mlflow
-from pathlib import Path
-from app.schemas import TripInput
+import os
+import pickle
+import requests
+
+def download_model_from_drive(file_id, dest_path):
+    print("Checking if model exists...")
+    if os.path.exists(dest_path):
+        print("Model already exists.")
+        return
+
+    print("Downloading model from Google Drive...")
+    url = f"https://drive.google.com/uc?export=download&id={file_id}"
+    response = requests.get(url)
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    with open(dest_path, "wb") as f:
+        f.write(response.content)
+    print("Model downloaded and saved to:", dest_path)
+
+# Set your Google Drive File ID here
+FILE_ID = "1VgjFrEtSTXAl3ks-dYLBaIe17bdpWSIk"  # <-- replace with yours
+MODEL_PATH = "model/model.pkl"
+
+# Download the model if not present
+download_model_from_drive(FILE_ID, MODEL_PATH)
+
+# Load the model
+with open(MODEL_PATH, "rb") as f:
+    model = pickle.load(f)
+
+def load_ridge_model():
+    return model
 
 
-try:
-    model = mlflow.sklearn.load_model("model")
-    print("✅ Model loaded successfully", model)
-except Exception as e:
-    print(f"❌ Error loading model: {str(e)}")
-
-def preprocess_input(data: TripInput):
-    try:
-        input_array = [
-            data.vendor_id,
-            data.passenger_count,
-            data.pickup_longitude,
-            data.pickup_latitude,
-            data.dropoff_longitude,
-            data.dropoff_latitude,
-            data.store_and_fwd_flag,
-            data.trip_hour,
-            data.is_peak_hour,
-            data.trip_dayofweek
-        ]
-        return input_array
-    except Exception as e:
-        print(f"❌ Error preprocessing input: {str(e)}")
-        return None
